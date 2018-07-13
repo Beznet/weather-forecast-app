a0b9ec75ec8079e099e83083180947da126e0308
Beznet <bennettdungan@gmail.com>
2018-07-12 22:37:20 -0500

    added JSON output functionality

--- index.js
+++ index.js
@@ -2,51 +2,6 @@ const getData = require('./include.js');
 const express = require('express')
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
-app.listen(9090, () => {
-  console.log('Example app listening on port 9090!')
-});
-
-getData(function(data){
-let array = new Array(data);
-// function for finding the Mean of an array
-function findMean([array]){
-  //reduce being used to add up the numbers in the array.
-  //a being the accmulated value and b being the current //value
-  return array.reduce((a, b) => a + b)/array.length;
-}
-
-//function for finding the Median of an array
-
-function findMedian([array]) {
-  //define median, set starting val to 0
-    var median = 0;
-  //define the array's length (self-explanatory)  
-    var length = array.length;
-    
-  //sorts the array from smallest to largest values
-    array.sort();
- 
-    if (
-        length % 2 === 0 // for even array
-    ) {
-        median = 
-        //finds lower low mid num then high mid num 
-        //then divides by 2 to get mean of the 2 middle //nums
-        (array[length / 2 - 1] + array[length / 2]) / 2;
-    } else { //for odd array
-        // middle number only
-        median = array[(length - 1) / 2];
-    }
- 
-    return median;
-}
-
-//function for finding the Mode(s) of an array
-
 function findMode([array]) {
 
     var modes = [];
@@ -74,12 +29,63 @@ function findMode([array]) {
     return modes;
 }
 
-console.log('Lafayette, LA 3 Day Weather Forecast-')
-console.log('Temperature Mean:' + ' ' + findMean(array) + ' ' + 'degrees'); 
+function findMean([array]){
+  //reduce being used to add up the numbers in the array.
+  //a being the accmulated value and b being the current value
+  return array.reduce((a, b) => a + b)/array.length;
+}
 
-console.log('Tmperature Median:' + ' ' + findMedian(array) + ' '+ 'degrees');
+//function for finding the Median of an array
 
-console.log('Temperature Mode(s):' + ' ' + findMode(array) + ' '+ 'degrees');
+function findMedian(array) {
+  //define median, set starting val to 0
+    var median = 0;
+  //define the array's length (self-explanatory)  
+    var length = array.length;
+    
+  //sorts the array from smallest to largest values
+    array.sort();
+ 
+    if (
+        length % 2 === 0 // for even array
+    ) {
+        median = 
+        //finds lower low mid num then high mid num 
+        //then divides by 2 to get mean of the 2 middle //nums
+        (array[length / 2 - 1] + array[length / 2]) / 2;
+    } else { //for odd array
+        // middle number only
+        median = array[(length - 1) / 2];
+    }
+ 
+    return median;
+}
 
-//console.log('Temperatures for the next three days every three hours:' + array);
-})
+
+app.listen(9090, () => {
+  console.log('Example app listening on port 9090!')
+});
+// function for finding the Mean of an array
+
+
+//function for finding the Mode(s) of an array
+
+// getData(function(data){
+// let array = new Array(data);
+
+app.get('/', (req, res) => {
+  let retVal = {};
+  getData(function(data){
+      let numbers = [data];
+      retVal.mode = findMode(numbers)
+      retVal.mean = findMean(numbers)
+      retVal.median = findMedian(numbers)
+      res.json(retVal)
+  })
+  // res.json('Lafayette, LA 3 Day Weather Forecast-');
+  // res.json('findMean(array)'); 
+
+  // res.json('Tmperature Median:' + ' ' + findMedian(array) + ' '+ 'degrees');
+
+  // res.json('Temperature Mode(s):' + ' ' + findMode(array) + ' '+ 'degrees');
+});
